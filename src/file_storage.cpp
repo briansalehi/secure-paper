39b597fdeac995ad51d21287d5e66a018ece15cb
Brian Salehi <salehibrian@gmail.com>
2022-08-29 08:00:47 +0430

    implement proper spacing in latex

--- src/file_storage.cpp
+++ src/file_storage.cpp
@@ -8,13 +8,11 @@ file_storage::file_storage(std::filesystem::path const& storage_path): storage_p
 
 void file_storage::write(printing_type type, unsigned int position)
 {
-    buffer << R"(\documentclass{article})" << "\n";
+    buffer << R"(\documentclass[a4paper,oneside,11pt]{article})" << "\n";
     buffer << R"(\begin{document})" << "\n";
     buffer << R"(\noindent)" << "\n";
-
-    for (int index = 1; index < position; ++index)
-        buffer << index << R"(.\\)" << std::endl;
-
+    buffer << R"(\begin{flushleft})" << "\n";
+    buffer << R"(\vspace*{)" << position * 11 << "pt}\n";
     buffer << R"(\expandafter\string\csname )";
     buffer << position << ". " << keys.username << "@" << keys.domain;
     buffer << R"( \endcsname)" << "\n";
@@ -22,7 +20,7 @@ void file_storage::write(printing_type type, unsigned int position)
     buffer << R"(\expandafter\string\csname )";
     buffer << keys.passphrase;
     buffer << R"( \endcsname)" << "\n";
-
+    buffer << R"(\end{flushleft})" << "\n";
     buffer << R"(\end{document})" << "\n";
 
     if (storage_path.empty())
